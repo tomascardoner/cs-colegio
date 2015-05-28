50ca0b4643513d059a6711dc75ffcbcd2bb485a4
tomascardoner <tomas@cardoner.com.ar>
2015-05-28 19:17:27 -0300

    Generacion de Facturas por Lote. Antes de simplificar los pasos de Verificacion y Emision en uno solo.

--- Desktop Application/formGenerarLoteFacturas.Designer.vb
+++ Desktop Application/formGenerarLoteFacturas.Designer.vb
@@ -78,11 +78,6 @@ Partial Class formGenerarLoteFacturas
         Me.columnDescripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.columnPrecioTotal = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.datagridviewPaso4Cabecera = New System.Windows.Forms.DataGridView()
-        Me.pictureboxPaso4 = New System.Windows.Forms.PictureBox()
-        Me.labelPaso4Mensaje = New System.Windows.Forms.Label()
-        Me.labelPaso4Titulo = New System.Windows.Forms.Label()
-        Me.buttonPaso4Anterior = New System.Windows.Forms.Button()
-        Me.buttonPaso4Siguiente = New System.Windows.Forms.Button()
         Me.columnComprobanteTipoNombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.columnComprobanteNumero = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.columnEntidadApellido = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -90,6 +85,11 @@ Partial Class formGenerarLoteFacturas
         Me.columnCUIT = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.columnCategoriaIVANombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.columnImporteTotal = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pictureboxPaso4 = New System.Windows.Forms.PictureBox()
+        Me.labelPaso4Mensaje = New System.Windows.Forms.Label()
+        Me.labelPaso4Titulo = New System.Windows.Forms.Label()
+        Me.buttonPaso4Anterior = New System.Windows.Forms.Button()
+        Me.buttonPaso4Siguiente = New System.Windows.Forms.Button()
         Me.panelPaso1.SuspendLayout()
         Me.tabcontrolMain.SuspendLayout()
         Me.tabpageNivelesCursosAlumnos.SuspendLayout()
@@ -621,66 +621,6 @@ Partial Class formGenerarLoteFacturas
         Me.datagridviewPaso4Cabecera.Size = New System.Drawing.Size(605, 208)
         Me.datagridviewPaso4Cabecera.TabIndex = 109
         '
-        'pictureboxPaso4
-        '
-        Me.pictureboxPaso4.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_COMPROBANTE_48
-        Me.pictureboxPaso4.Location = New System.Drawing.Point(3, 20)
-        Me.pictureboxPaso4.Name = "pictureboxPaso4"
-        Me.pictureboxPaso4.Size = New System.Drawing.Size(48, 48)
-        Me.pictureboxPaso4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
-        Me.pictureboxPaso4.TabIndex = 108
-        Me.pictureboxPaso4.TabStop = False
-        '
-        'labelPaso4Mensaje
-        '
-        Me.labelPaso4Mensaje.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.labelPaso4Mensaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelPaso4Mensaje.Location = New System.Drawing.Point(57, 20)
-        Me.labelPaso4Mensaje.Name = "labelPaso4Mensaje"
-        Me.labelPaso4Mensaje.Size = New System.Drawing.Size(551, 61)
-        Me.labelPaso4Mensaje.TabIndex = 107
-        Me.labelPaso4Mensaje.Text = "Se han Emitido las siguientes facturas. Verifique que estén correcto tanto el enc" & _
-    "abezado como el detalle, y luego proceda a la Transmisión de los datos a través " & _
-    "de Internet."
-        '
-        'labelPaso4Titulo
-        '
-        Me.labelPaso4Titulo.AutoSize = True
-        Me.labelPaso4Titulo.Dock = System.Windows.Forms.DockStyle.Top
-        Me.labelPaso4Titulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelPaso4Titulo.Location = New System.Drawing.Point(0, 0)
-        Me.labelPaso4Titulo.Name = "labelPaso4Titulo"
-        Me.labelPaso4Titulo.Size = New System.Drawing.Size(124, 17)
-        Me.labelPaso4Titulo.TabIndex = 106
-        Me.labelPaso4Titulo.Text = "Paso 4: Emisión"
-        '
-        'buttonPaso4Anterior
-        '
-        Me.buttonPaso4Anterior.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.buttonPaso4Anterior.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_MOVE_PREVIOUS_24
-        Me.buttonPaso4Anterior.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.buttonPaso4Anterior.Location = New System.Drawing.Point(322, 404)
-        Me.buttonPaso4Anterior.Name = "buttonPaso4Anterior"
-        Me.buttonPaso4Anterior.Size = New System.Drawing.Size(140, 34)
-        Me.buttonPaso4Anterior.TabIndex = 99
-        Me.buttonPaso4Anterior.Text = "Paso 3: Confirmación"
-        Me.buttonPaso4Anterior.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.buttonPaso4Anterior.UseVisualStyleBackColor = True
-        '
-        'buttonPaso4Siguiente
-        '
-        Me.buttonPaso4Siguiente.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.buttonPaso4Siguiente.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_MOVE_NEXT_24
-        Me.buttonPaso4Siguiente.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.buttonPaso4Siguiente.Location = New System.Drawing.Point(468, 404)
-        Me.buttonPaso4Siguiente.Name = "buttonPaso4Siguiente"
-        Me.buttonPaso4Siguiente.Size = New System.Drawing.Size(140, 34)
-        Me.buttonPaso4Siguiente.TabIndex = 98
-        Me.buttonPaso4Siguiente.Text = "Paso 5: Transmisión"
-        Me.buttonPaso4Siguiente.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.buttonPaso4Siguiente.UseVisualStyleBackColor = True
-        '
         'columnComprobanteTipoNombre
         '
         Me.columnComprobanteTipoNombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
@@ -753,6 +693,66 @@ Partial Class formGenerarLoteFacturas
         Me.columnImporteTotal.ReadOnly = True
         Me.columnImporteTotal.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
         Me.columnImporteTotal.Width = 48
+        '
+        'pictureboxPaso4
+        '
+        Me.pictureboxPaso4.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_COMPROBANTE_48
+        Me.pictureboxPaso4.Location = New System.Drawing.Point(3, 20)
+        Me.pictureboxPaso4.Name = "pictureboxPaso4"
+        Me.pictureboxPaso4.Size = New System.Drawing.Size(48, 48)
+        Me.pictureboxPaso4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pictureboxPaso4.TabIndex = 108
+        Me.pictureboxPaso4.TabStop = False
+        '
+        'labelPaso4Mensaje
+        '
+        Me.labelPaso4Mensaje.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.labelPaso4Mensaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelPaso4Mensaje.Location = New System.Drawing.Point(57, 20)
+        Me.labelPaso4Mensaje.Name = "labelPaso4Mensaje"
+        Me.labelPaso4Mensaje.Size = New System.Drawing.Size(551, 61)
+        Me.labelPaso4Mensaje.TabIndex = 107
+        Me.labelPaso4Mensaje.Text = "Se han Emitido las siguientes facturas. Verifique que estén correcto tanto el enc" & _
+    "abezado como el detalle, y luego proceda a la Transmisión de los datos a través " & _
+    "de Internet."
+        '
+        'labelPaso4Titulo
+        '
+        Me.labelPaso4Titulo.AutoSize = True
+        Me.labelPaso4Titulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.labelPaso4Titulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelPaso4Titulo.Location = New System.Drawing.Point(0, 0)
+        Me.labelPaso4Titulo.Name = "labelPaso4Titulo"
+        Me.labelPaso4Titulo.Size = New System.Drawing.Size(124, 17)
+        Me.labelPaso4Titulo.TabIndex = 106
+        Me.labelPaso4Titulo.Text = "Paso 4: Emisión"
+        '
+        'buttonPaso4Anterior
+        '
+        Me.buttonPaso4Anterior.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonPaso4Anterior.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_MOVE_PREVIOUS_24
+        Me.buttonPaso4Anterior.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.buttonPaso4Anterior.Location = New System.Drawing.Point(322, 404)
+        Me.buttonPaso4Anterior.Name = "buttonPaso4Anterior"
+        Me.buttonPaso4Anterior.Size = New System.Drawing.Size(140, 34)
+        Me.buttonPaso4Anterior.TabIndex = 99
+        Me.buttonPaso4Anterior.Text = "Paso 3: Confirmación"
+        Me.buttonPaso4Anterior.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.buttonPaso4Anterior.UseVisualStyleBackColor = True
+        '
+        'buttonPaso4Siguiente
+        '
+        Me.buttonPaso4Siguiente.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonPaso4Siguiente.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_MOVE_NEXT_24
+        Me.buttonPaso4Siguiente.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.buttonPaso4Siguiente.Location = New System.Drawing.Point(468, 404)
+        Me.buttonPaso4Siguiente.Name = "buttonPaso4Siguiente"
+        Me.buttonPaso4Siguiente.Size = New System.Drawing.Size(140, 34)
+        Me.buttonPaso4Siguiente.TabIndex = 98
+        Me.buttonPaso4Siguiente.Text = "Paso 5: Transmisión"
+        Me.buttonPaso4Siguiente.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.buttonPaso4Siguiente.UseVisualStyleBackColor = True
         '
         'formGenerarLoteFacturas
         '
