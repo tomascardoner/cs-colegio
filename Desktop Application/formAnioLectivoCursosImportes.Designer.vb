4fac3ee3831f07094b45c669d7574a7b0b8f5b34
Tomas A. Cardoner <tomas@cardoner.com.ar>
2018-10-29 13:10:31 -0300

    No sé

---
+++ Desktop Application/formAnioLectivoCursosImportes.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formAnioLectivoCursosImportes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle11 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle12 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle13 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle14 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle15 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.buttonGuardar = New System.Windows.Forms.ToolStripButton()
+        Me.buttonCancelar = New System.Windows.Forms.ToolStripButton()
+        Me.toolstripMain = New System.Windows.Forms.ToolStrip()
+        Me.labelMesInicio = New System.Windows.Forms.Label()
+        Me.labelAnioLectivoOrigen = New System.Windows.Forms.Label()
+        Me.datagridviewMain = New System.Windows.Forms.DataGridView()
+        Me.comboboxMesInicio = New System.Windows.Forms.ComboBox()
+        Me.comboboxAnioLectivo = New System.Windows.Forms.ComboBox()
+        Me.columnNivel = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnCurso = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnImporteMatricula = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnImporteCuota = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.buttonEditar = New System.Windows.Forms.ToolStripButton()
+        Me.buttonCerrar = New System.Windows.Forms.ToolStripButton()
+        Me.toolstripMain.SuspendLayout()
+        CType(Me.datagridviewMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'buttonGuardar
+        '
+        Me.buttonGuardar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.buttonGuardar.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_OK_32
+        Me.buttonGuardar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.buttonGuardar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.buttonGuardar.Name = "buttonGuardar"
+        Me.buttonGuardar.Size = New System.Drawing.Size(85, 36)
+        Me.buttonGuardar.Text = "Guardar"
+        '
+        'buttonCancelar
+        '
+        Me.buttonCancelar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.buttonCancelar.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_CANCEL_32
+        Me.buttonCancelar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.buttonCancelar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.buttonCancelar.Name = "buttonCancelar"
+        Me.buttonCancelar.Size = New System.Drawing.Size(89, 36)
+        Me.buttonCancelar.Text = "Cancelar"
+        '
+        'toolstripMain
+        '
+        Me.toolstripMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.toolstripMain.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.toolstripMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.buttonCerrar, Me.buttonEditar, Me.buttonCancelar, Me.buttonGuardar})
+        Me.toolstripMain.Location = New System.Drawing.Point(0, 0)
+        Me.toolstripMain.Name = "toolstripMain"
+        Me.toolstripMain.Size = New System.Drawing.Size(498, 39)
+        Me.toolstripMain.TabIndex = 5
+        '
+        'labelMesInicio
+        '
+        Me.labelMesInicio.AutoSize = True
+        Me.labelMesInicio.Location = New System.Drawing.Point(190, 52)
+        Me.labelMesInicio.Name = "labelMesInicio"
+        Me.labelMesInicio.Size = New System.Drawing.Size(73, 13)
+        Me.labelMesInicio.TabIndex = 2
+        Me.labelMesInicio.Text = "Mes de Inicio:"
+        '
+        'labelAnioLectivoOrigen
+        '
+        Me.labelAnioLectivoOrigen.AutoSize = True
+        Me.labelAnioLectivoOrigen.Location = New System.Drawing.Point(10, 52)
+        Me.labelAnioLectivoOrigen.Name = "labelAnioLectivoOrigen"
+        Me.labelAnioLectivoOrigen.Size = New System.Drawing.Size(67, 13)
+        Me.labelAnioLectivoOrigen.TabIndex = 0
+        Me.labelAnioLectivoOrigen.Text = "Año Lectivo:"
+        '
+        'datagridviewMain
+        '
+        Me.datagridviewMain.AllowUserToAddRows = False
+        Me.datagridviewMain.AllowUserToDeleteRows = False
+        Me.datagridviewMain.AllowUserToResizeRows = False
+        DataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        DataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.MenuHighlight
+        DataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.WindowText
+        Me.datagridviewMain.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle11
+        Me.datagridviewMain.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.datagridviewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.datagridviewMain.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.columnNivel, Me.columnCurso, Me.columnImporteMatricula, Me.columnImporteCuota})
+        Me.datagridviewMain.Location = New System.Drawing.Point(12, 76)
+        Me.datagridviewMain.MultiSelect = False
+        Me.datagridviewMain.Name = "datagridviewMain"
+        Me.datagridviewMain.RowHeadersVisible = False
+        Me.datagridviewMain.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.datagridviewMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.datagridviewMain.Size = New System.Drawing.Size(476, 244)
+        Me.datagridviewMain.TabIndex = 4
+        '
+        'comboboxMesInicio
+        '
+        Me.comboboxMesInicio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboboxMesInicio.FormattingEnabled = True
+        Me.comboboxMesInicio.Location = New System.Drawing.Point(269, 49)
+        Me.comboboxMesInicio.Name = "comboboxMesInicio"
+        Me.comboboxMesInicio.Size = New System.Drawing.Size(118, 21)
+        Me.comboboxMesInicio.TabIndex = 3
+        '
+        'comboboxAnioLectivo
+        '
+        Me.comboboxAnioLectivo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboboxAnioLectivo.FormattingEnabled = True
+        Me.comboboxAnioLectivo.Location = New System.Drawing.Point(83, 49)
+        Me.comboboxAnioLectivo.Name = "comboboxAnioLectivo"
+        Me.comboboxAnioLectivo.Size = New System.Drawing.Size(59, 21)
+        Me.comboboxAnioLectivo.TabIndex = 6
+        '
+        'columnNivel
+        '
+        Me.columnNivel.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.columnNivel.DataPropertyName = "NivelNombre"
+        DataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        Me.columnNivel.DefaultCellStyle = DataGridViewCellStyle12
+        Me.columnNivel.HeaderText = "Nivel"
+        Me.columnNivel.Name = "columnNivel"
+        Me.columnNivel.ReadOnly = True
+        Me.columnNivel.Width = 56
+        '
+        'columnCurso
+        '
+        Me.columnCurso.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.columnCurso.DataPropertyName = "CursoNombre"
+        DataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        Me.columnCurso.DefaultCellStyle = DataGridViewCellStyle13
+        Me.columnCurso.HeaderText = "Curso"
+        Me.columnCurso.Name = "columnCurso"
+        Me.columnCurso.ReadOnly = True
+        Me.columnCurso.Width = 59
+        '
+        'columnImporteMatricula
+        '
+        Me.columnImporteMatricula.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.columnImporteMatricula.DataPropertyName = "ImporteMatricula"
+        DataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle14.Format = "C2"
+        DataGridViewCellStyle14.NullValue = Nothing
+        Me.columnImporteMatricula.DefaultCellStyle = DataGridViewCellStyle14
+        Me.columnImporteMatricula.HeaderText = "Importe Matrícula"
+        Me.columnImporteMatricula.Name = "columnImporteMatricula"
+        Me.columnImporteMatricula.Width = 105
+        '
+        'columnImporteCuota
+        '
+        Me.columnImporteCuota.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.columnImporteCuota.DataPropertyName = "ImporteCuota"
+        DataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle15.Format = "C2"
+        DataGridViewCellStyle15.NullValue = Nothing
+        Me.columnImporteCuota.DefaultCellStyle = DataGridViewCellStyle15
+        Me.columnImporteCuota.HeaderText = "Importe Cuota"
+        Me.columnImporteCuota.Name = "columnImporteCuota"
+        Me.columnImporteCuota.Width = 90
+        '
+        'buttonEditar
+        '
+        Me.buttonEditar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.buttonEditar.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_ITEM_EDIT_32
+        Me.buttonEditar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.buttonEditar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.buttonEditar.Name = "buttonEditar"
+        Me.buttonEditar.Size = New System.Drawing.Size(73, 36)
+        Me.buttonEditar.Text = "Editar"
+        '
+        'buttonCerrar
+        '
+        Me.buttonCerrar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.buttonCerrar.Image = Global.CSColegio.DesktopApplication.My.Resources.Resources.IMAGE_CLOSE_32
+        Me.buttonCerrar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.buttonCerrar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.buttonCerrar.Name = "buttonCerrar"
+        Me.buttonCerrar.Size = New System.Drawing.Size(75, 36)
+        Me.buttonCerrar.Text = "Cerrar"
+        '
+        'formAnioLectivoCursosImportes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(498, 330)
+        Me.Controls.Add(Me.comboboxAnioLectivo)
+        Me.Controls.Add(Me.comboboxMesInicio)
+        Me.Controls.Add(Me.datagridviewMain)
+        Me.Controls.Add(Me.labelMesInicio)
+        Me.Controls.Add(Me.labelAnioLectivoOrigen)
+        Me.Controls.Add(Me.toolstripMain)
+        Me.KeyPreview = True
+        Me.Name = "formAnioLectivoCursosImportes"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Año Lectivo - Cursos - Editar Importes"
+        Me.toolstripMain.ResumeLayout(False)
+        Me.toolstripMain.PerformLayout()
+        CType(Me.datagridviewMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents buttonGuardar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents buttonCancelar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents toolstripMain As System.Windows.Forms.ToolStrip
+    Friend WithEvents labelMesInicio As System.Windows.Forms.Label
+    Friend WithEvents labelAnioLectivoOrigen As System.Windows.Forms.Label
+    Friend WithEvents datagridviewMain As System.Windows.Forms.DataGridView
+    Friend WithEvents comboboxMesInicio As System.Windows.Forms.ComboBox
+    Friend WithEvents comboboxAnioLectivo As System.Windows.Forms.ComboBox
+    Friend WithEvents columnNivel As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnCurso As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnImporteMatricula As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnImporteCuota As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents buttonEditar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents buttonCerrar As System.Windows.Forms.ToolStripButton
+End Class
---
+++ Desktop Application/formAnioLectivoCursosImportes.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Globalization
+
+Public Class formAnioLectivoCursosImportes
+
+#Region "Declarations"
+    Private mdbContext As New CSColegioContext(True)
+
+    Private Class GridRowData
+        Public Property IDAnioLectivoCurso As Short
+        Public Property NivelNombre As String
+        Public Property CursoNombre As String
+        Public Property ImporteMatricula As Decimal
+        Public Property ImporteCuota As Decimal
+    End Class
+
+    Private mlistGridRowData As List(Of GridRowData)
+    Private mlistAnioLectivoCursoImporte As List(Of AnioLectivoCursoImporte)
+
+    Private mSkipFilterData As Boolean = False
+    Private mEditMode As Boolean = False
+#End Region
+
+#Region "Form stuff"
+    Private Sub ChangeMode()
+        buttonGuardar.Visible = mEditMode
+        buttonCancelar.Visible = mEditMode
+        buttonEditar.Visible = (mEditMode = False)
+        buttonCerrar.Visible = (mEditMode = False)
+
+        comboboxAnioLectivo.Enabled = Not mEditMode
+        comboboxMesInicio.Enabled = Not mEditMode
+
+        columnImporteMatricula.ReadOnly = Not mEditMode
+        columnImporteCuota.ReadOnly = Not mEditMode
+    End Sub
+
+    Friend Sub SetAppearance()
+        datagridviewMain.DefaultCellStyle.Font = My.Settings.GridsAndListsFont
+        datagridviewMain.ColumnHeadersDefaultCellStyle.Font = My.Settings.GridsAndListsFont
+    End Sub
+
+    Private Sub Me_Load() Handles Me.Load
+        SetAppearance()
+
+        mSkipFilterData = True
+
+        ' Cargo los ComboBox
+        pFillAndRefreshLists.AnioLectivo(comboboxAnioLectivo, False, SortOrder.Ascending)
+        comboboxAnioLectivo.SelectedIndex = comboboxAnioLectivo.FindStringExact(DateTime.Today.Year.ToString)
+        pFillAndRefreshLists.Mes(comboboxMesInicio, True, False, True, False, False)
+        comboboxMesInicio.SelectedIndex = 0
+
+        mSkipFilterData = False
+
+        ChangeMode()
+
+        SetDataFromObjectToControls()
+    End Sub
+
+    Private Sub Me_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        mdbContext.Dispose()
+        mdbContext = Nothing
+        mlistGridRowData = Nothing
+        mlistAnioLectivoCursoImporte = Nothing
+        Me.Dispose()
+    End Sub
+#End Region
+
+#Region "Load and Set Data"
+    Friend Sub SetDataFromObjectToControls()
+        Dim AnioLectivo As Short
+        Dim MesInicio As Byte
+        Dim AnioLectivoCursoImporteCurrent As AnioLectivoCursoImporte
+
+        If mSkipFilterData Then
+            Exit Sub
+        End If
+
+        AnioLectivo = Convert.ToInt16(comboboxAnioLectivo.Text)
+        MesInicio = Convert.ToByte(comboboxMesInicio.SelectedIndex + 1)
+        mlistAnioLectivoCursoImporte = New List(Of AnioLectivoCursoImporte)
+
+        For Each AnioLectivoCursoCurrent In mdbContext.AnioLectivoCurso.Where(Function(alc) alc.AnioLectivo = AnioLectivo)
+            AnioLectivoCursoImporteCurrent = mdbContext.AnioLectivoCursoImporte.Find(AnioLectivoCursoCurrent.IDAnioLectivoCurso, MesInicio)
+
+            If AnioLectivoCursoImporteCurrent Is Nothing Then
+                AnioLectivoCursoImporteCurrent = New AnioLectivoCursoImporte
+
+                AnioLectivoCursoImporteCurrent.IDAnioLectivoCurso = AnioLectivoCursoCurrent.IDAnioLectivoCurso
+                AnioLectivoCursoImporteCurrent.MesInicio = MesInicio
+
+                mdbContext.AnioLectivoCursoImporte.Add(AnioLectivoCursoImporteCurrent)
+            End If
+            mlistAnioLectivoCursoImporte.Add(AnioLectivoCursoImporteCurrent)
+        Next
+
+        mlistGridRowData = (From alci In mlistAnioLectivoCursoImporte
+                            Join alc In mdbContext.AnioLectivoCurso On alci.IDAnioLectivoCurso Equals alc.IDAnioLectivoCurso
+                            Join c In mdbContext.Curso On c.IDCurso Equals alc.IDCurso
+                            Join a In mdbContext.Anio On a.IDAnio Equals c.IDAnio
+                            Join n In mdbContext.Nivel On n.IDNivel Equals a.IDNivel
+                            Join t In mdbContext.Turno On c.IDTurno Equals t.IDTurno()
+                            Order By n.Nombre, a.Nombre, n.Nombre
+                            Where alc.AnioLectivo = AnioLectivo And alci.MesInicio = MesInicio
+                            Select New GridRowData With {.IDAnioLectivoCurso = alc.IDAnioLectivoCurso, .NivelNombre = n.Nombre, .CursoNombre = a.Nombre & " - " & t.Nombre & " - " & c.Division, .ImporteMatricula = alci.ImporteMatricula, .ImporteCuota = alci.ImporteCuota}).ToList
+
+        datagridviewMain.AutoGenerateColumns = False
+        datagridviewMain.DataSource = mlistGridRowData
+    End Sub
+
+    Friend Sub SetDataFromControlsToObject()
+        For Each GridRowDataActual As GridRowData In mlistGridRowData
+            Dim AnioLectivoCursoImporteCurrent As AnioLectivoCursoImporte
+
+            AnioLectivoCursoImporteCurrent = mlistAnioLectivoCursoImporte.Find(Function(alci) alci.IDAnioLectivoCurso = GridRowDataActual.IDAnioLectivoCurso)
+            With AnioLectivoCursoImporteCurrent
+                .ImporteMatricula = GridRowDataActual.ImporteMatricula
+                .ImporteCuota = GridRowDataActual.ImporteCuota
+            End With
+        Next
+    End Sub
+#End Region
+
+#Region "Controls behavior"
+    Private Sub FormKeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        Select Case e.KeyChar
+            Case Microsoft.VisualBasic.ChrW(Keys.Return)
+                buttonGuardar.PerformClick()
+            Case Microsoft.VisualBasic.ChrW(Keys.Escape)
+                buttonCancelar.PerformClick()
+        End Select
+    End Sub
+
+    Private Sub AnioLectivoCambio(sender As Object, e As EventArgs) Handles comboboxAnioLectivo.SelectedIndexChanged
+        SetDataFromObjectToControls()
+    End Sub
+
+    Private Sub MesInicioCambio(sender As Object, e As EventArgs) Handles comboboxMesInicio.SelectedIndexChanged
+        SetDataFromObjectToControls()
+    End Sub
+
+    Private Sub Grid_CellValidating(ByVal sender As Object, ByVal e As DataGridViewCellValidatingEventArgs) Handles datagridviewMain.CellValidating
+        Dim style As NumberStyles
+        Dim importe As Decimal
+
+        If Not mEditMode Then
+            Return
+        End If
+
+        ' Don't try to validate the 'new row' until finished 
+        ' editing since there is not any point in validating its initial value.
+        If datagridviewMain.Rows(e.RowIndex).IsNewRow Then
+            Return
+        End If
+
+        datagridviewMain.Rows(e.RowIndex).ErrorText = ""
+
+        style = NumberStyles.Number Or NumberStyles.AllowCurrencySymbol
+
+        If e.ColumnIndex > 1 AndAlso Not Decimal.TryParse(e.FormattedValue.ToString(), style, CultureInfo.CurrentCulture, importe) OrElse importe < 0 Then
+            e.Cancel = True
+            datagridviewMain.Rows(e.RowIndex).ErrorText = "El importe debe ser un valor numérico mayor o igual a cero."
+            MsgBox("El importe debe ser un valor numérico mayor o igual a cero.", vbInformation, My.Application.Info.Title)
+        End If
+    End Sub
+
+    Private Sub Grid_DataError(sender As Object, e As DataGridViewDataErrorEventArgs) Handles datagridviewMain.DataError
+        MsgBox("El importe debe ser un valor numérico mayor o igual a cero.", vbInformation, My.Application.Info.Title)
+        e.ThrowException = False
+    End Sub
+#End Region
+
+#Region "Main Toolbar"
+    Private Sub buttonEditar_Click() Handles buttonEditar.Click
+        If Permisos.VerificarPermiso(Permisos.ANIOLECTIVOCURSOIMPORTE_EDITAR) Then
+            mEditMode = True
+            ChangeMode()
+        End If
+    End Sub
+
+    Private Sub buttonCerrarOCancelar_Click() Handles buttonCerrar.Click, buttonCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub buttonCancelar_Click() Handles buttonCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub buttonGuardar_Click() Handles buttonGuardar.Click
+        ' Paso los datos desde los controles al Objecto de EF
+        SetDataFromControlsToObject()
+
+        If mdbContext.ChangeTracker.HasChanges Then
+
+            Me.Cursor = Cursors.WaitCursor
+
+            Try
+                ' Guardo los cambios
+                mdbContext.SaveChanges()
+
+                mEditMode = False
+
+                ChangeMode()
+
+                Me.Cursor = Cursors.Default
+
+            Catch dbuex As System.Data.Entity.Infrastructure.DbUpdateException
+                Me.Cursor = Cursors.Default
+                Select Case CS_Database_EF_SQL.TryDecodeDbUpdateException(dbuex)
+                    Case Errors.DuplicatedEntity
+                        MsgBox("No se pueden guardar los cambios porque ya existe un Anio Lectivo y Curso - Importe con los mismos datos.", MsgBoxStyle.Exclamation, My.Application.Info.Title)
+                End Select
+                Exit Sub
+
+            Catch ex As Exception
+                Me.Cursor = Cursors.Default
+                CS_Error.ProcessError(ex, My.Resources.STRING_ERROR_SAVING_CHANGES)
+                Exit Sub
+            End Try
+        End If
+    End Sub
+#End Region
+
+End Class
